11e261b6c8d425935c2c3b581be05dc362034b6f
le-dev-guy <imlokesh.200@gmail.com>
2022-11-12 15:09:16 +0530

    Converted API responsed from  CSV to JSON

---
+++ src/Components/Stocks/utils/csvtojson.js
@@ -0,0 +1,22 @@
+export const csvToJSON = (csv, setRows) => {
+  const lines = csv.split('\n');
+  const result = [];
+  let headers;
+  headers = lines[0].split(',');
+
+  for (var i = 1; i < lines.length; i++) {
+    var obj = {};
+
+    if (lines[i] == undefined || lines[i].trim() == '') {
+      continue;
+    }
+
+    var words = lines[i].split(',');
+    for (var j = 0; j < words.length; j++) {
+      obj[headers[j].trim()] = words[j];
+    }
+    result.push(obj);
+  }
+  result.push();
+  setRows(result);
+};
